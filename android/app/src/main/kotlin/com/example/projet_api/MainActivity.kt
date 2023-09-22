98450193d5add95d2758673d3af5723808cda729
cjulliar <cyrjulliard@gmail.com>
2023-09-22 10:06:38 +0200

    Revert "ajout du projet firstApi"
    This reverts commit 13d072ff57aa98938366059b940f53a5609de707.

--- android/app/src/main/kotlin/com/example/projet_api/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.projet_api
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/private_access.dart
+++
@@ -1,1 +0,0 @@
-String apiKey = '86a929392f35a23f1ffc978dbf15ecb4';
